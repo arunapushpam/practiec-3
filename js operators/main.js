be4df38ebbc943b563eb26d24fe98392ce874f2b
Anitha <arunaani2000@gmail.com>
2022-10-23 21:37:06 +0530

    add folder

---
+++ js operators/main.js
@@ -0,0 +1,67 @@
+	//<h3>operators</h3> 
+	//<p>Arithmetic operators =,-,%,*,**
+	//Assignment operators
+	//Comparison operators
+	//Logical operators
+	//Bitwise operators</p> 
+	{
+	var a=5;
+	b=a;
+	console.log(b);
+	}
+	
+	{
+	var a=5;
+	a+=3;
+	console.log(a);
+	}
+
+	{
+	var a=5;
+	a/=3;
+	console.log(a);
+	}
+	
+	{
+		var num1=7,num2=9;
+		sum=(num1+num2);
+		console.log(sum);
+		
+		var num1=7,num2=8;
+		console.log(num1+num2);
+	}
+	{
+		var num1=7,num2=8;
+		console.log(num1**num2);
+	}
+	{
+		var num1=15
+		console.log(++num1)
+		console.log(++num1)
+		
+		var num1=8
+		console.log(num1++)
+		console.log(num1)
+		
+		var num1=5
+		console.log(--num1)
+		console.log(--num1)
+		
+		var num1=2
+		console.log(num1--)
+		console.log(num1--)
+	}
+	{
+		var numa=3,	numb="5"
+		console.log(numa+numb)
+		
+	}
+	{
+		let a=10,b=7
+		console.log(a>b)	
+	}
+	{
+		let a=10,b=7
+		console.log(a==b)	
+	}
+	
